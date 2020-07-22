16bad590893fe32996faa9394c7a0e1043b2f856
fonzerelly <christian.hoerauf@gmail.com>
2020-07-22 14:05:20 +0200

    implements mkdir per step

--- convert.sh
+++ convert.sh
@@ -1,8 +1,16 @@
+rm -rf output
+mkdir output
+export previousDir=from_scratch
+
+. ./helper.sh
 
 for step in steps/*.sh; do
-    for f in from_scratch/*.svg; do 
-    echo "Processing $f file.."
-
+    createOutputDir "output" $step
+    mkdir --parents $_RESULT_createOutputDir
+    for f in $previousDir/*.svg; do 
+    # echo "Processing $f file.."
+        
         $step $f
     done
+    export previousDir="output/"+$step
 done
---
+++ steps/01_remove_border.sh
@@ -0,0 +1,4 @@
+echo "remove border"
+echo $1
+# mkdir output/$0
+# inkscape --select
